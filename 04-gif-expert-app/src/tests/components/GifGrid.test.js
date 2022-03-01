04f2db5b291133ae09dfa9a75f26f9bb24a5bc21
ElfGod <albertogx@icloud.com>
2022-02-28 22:36:33 -0500

    43: Evaluar si existen componentes

--- 04-gif-expert-app/src/tests/components/GifGrid.test.js
+++ 04-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '@testing-library/jest-dom'
 import { shallow } from 'enzyme'
 import GifGrid from '../../components/GifGrid'
 import { useFetchGifs } from '../../hooks/useFetchGifs'
@@ -25,6 +26,11 @@ describe('Pruebas en el <GifGrid />', () => {
         url: 'https://localhost/cualquier/cosa.jpg',
         title: 'Cualquier cosa',
       },
+      // {
+      //   id: 'DEF',
+      //   url: 'https://localhost/cualquier/cosa.jpg',
+      //   title: 'Cualquier cosa',
+      // },
     ]
 
     useFetchGifs.mockReturnValue({
@@ -32,7 +38,12 @@ describe('Pruebas en el <GifGrid />', () => {
       loading: false,
     })
     const wrapper = shallow(<GifGrid category={category} />)
-
     expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('p').exists()).toBe(false)
+    // esperaria que mis gifs fuera 1, si le paso 2 fallaria
+    expect(wrapper.find('GifGridItem').length).toBe(1)
+    // esta prueba me funciona con cualquier cantidad de gifs le envie,
+    // sino hubiera ningun gifs me fallaria
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length)
   })
 })
