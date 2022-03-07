2b9e50472b24e61af878cad2ef30d25c477cd754
ElfGod <albertogx@icloud.com>
2022-03-06 20:40:03 -0500

    64: Agregar un nuevo TODO

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -12,8 +12,23 @@ const initialState = [
 ]
 
 const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState)
+  const [todos, dispatch] = useReducer(todoReducer, initialState)
   console.log(todos)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log('Nueva tarea')
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: 'Nueva tarea',
+      done: false,
+    }
+    const action = {
+      type: 'add',
+      payload: newTodo,
+    }
+    dispatch(action)
+  }
   return (
     <>
       <h1>TodoApp ({todos.length})</h1>
@@ -35,7 +50,7 @@ const TodoApp = () => {
         <div className='col-5 d-grid gap-2'>
           <h4>Agregar TODO</h4>
           <hr />
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type='text'
               name='description'
@@ -44,7 +59,10 @@ const TodoApp = () => {
               autoComplete='off'
             />
             <div className='d-grid gap-2'>
-              <button className='btn btn-outline-primary mt-2 btn-block'>
+              <button
+                type='submit'
+                className='btn btn-outline-primary mt-2 btn-block'
+              >
                 Agregar
               </button>
             </div>
--- 05-hook-app/src/components/08-useReducer/todoReducer.js
+++ 05-hook-app/src/components/08-useReducer/todoReducer.js
@@ -1,8 +1,7 @@
 export const todoReducer = (state = [], action) => {
   switch (action.type) {
-    // case 'add':
-
-    //   break;
+    case 'add':
+      return [...state, action.payload]
 
     default:
       return state
