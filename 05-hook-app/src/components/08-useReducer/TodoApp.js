d2372973374d2da0b8ec992a08c195d4779cb71b
ElfGod <albertogx@icloud.com>
2022-03-06 19:57:40 -0500

    63: Creando el cascaron de la lista de TODOs

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -6,7 +6,7 @@ import './styles.css'
 const initialState = [
   {
     id: new Date().getTime(),
-    des: 'Aprender React',
+    desc: 'Aprender React',
     done: false,
   },
 ]
@@ -16,13 +16,41 @@ const TodoApp = () => {
   console.log(todos)
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ({todos.length})</h1>
       <hr />
-      <ul>
-        <li>Hola</li>
-        <li>mundo</li>
-        <li>Hola de nuevo</li>
-      </ul>
+      <div className='row'>
+        <div className='col-7'>
+          <ul className='list-group list-group-flush'>
+            {todos.map((todo, i) => (
+              <li key={todo.id} className='list-group-item'>
+                <p className='text-center'>
+                  {' '}
+                  {i + 1}. {todo.desc}
+                </p>
+                <button className='btn btn-danger'>Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='col-5 d-grid gap-2'>
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form>
+            <input
+              type='text'
+              name='description'
+              className='form-control'
+              placeholder='Aprender ...'
+              autoComplete='off'
+            />
+            <div className='d-grid gap-2'>
+              <button className='btn btn-outline-primary mt-2 btn-block'>
+                Agregar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
