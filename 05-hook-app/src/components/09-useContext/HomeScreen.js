8059cb84bf1031a953b0e23c61d3e3490e717c26
ElfGod <albertogx@icloud.com>
2022-03-07 23:57:57 -0500

    71: CreateContext y useContext

--- 05-hook-app/src/components/09-useContext/HomeScreen.js
+++ 05-hook-app/src/components/09-useContext/HomeScreen.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UseContext'
 
 const HomeScreen = () => {
+  const userContext = useContext(UserContext)
+  console.log(userContext)
   return (
     <>
       <h1>HomeScreen</h1>
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import AppRouter from './AppRouter'
+import { UserContext } from './UseContext'
 
 const MainApp = () => {
-  return <AppRouter />
+  const user = {
+    id: 1234,
+    name: 'elfGod',
+    email: 'elfgod@elfgod.com',
+  }
+
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  )
 }
 
 export default MainApp
