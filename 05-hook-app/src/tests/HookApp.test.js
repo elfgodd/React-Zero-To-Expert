6910d14c2ca2dc2f70b4c6776639400e5dbef503
ElfGod <albertogx@icloud.com>
2022-03-08 20:01:15 -0500

    73: Inicio de proyecto - Pruebas sobre Hooks

---
+++ 05-hook-app/src/tests/HookApp.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import HookApp from '../HookApp'
+
+describe('Pruebas en <HookApp />', () => {
+  test('debe mostrarse correctamente', () => {
+    const wrapper = shallow(<HookApp />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
