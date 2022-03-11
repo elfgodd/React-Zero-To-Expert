78c9d13ca86298c3cf5358db8a20cfb5e21decf2
ElfGod <albertogx@icloud.com>
2022-03-11 15:06:52 -0500

    84: Pruebas sobre el Reducer

---
+++ 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
@@ -0,0 +1,26 @@
+import { todoReducer } from '../../../components/08-useReducer/todoReducer'
+import { demoTodos } from '../../fixtures/demoTodos'
+
+describe('Pruebas en todoReducer', () => {
+  test('debe de retornar el estado por defecto', () => {
+    const state = todoReducer(demoTodos, {})
+
+    expect(state).toEqual(demoTodos)
+  })
+  test('debe de agregar un TODO', () => {
+    const newTodo = {
+      id: 3,
+      desc: 'Aprender ExpressJS',
+      done: false,
+    }
+
+    const action = {
+      type: 'add',
+      payload: newTodo,
+    }
+
+    const state = todoReducer(demoTodos, action)
+    expect(state.length).toBe(3)
+    expect(state).toEqual([...demoTodos, newTodo])
+  })
+})
---
+++ 05-hook-app/src/tests/fixtures/demoTodos.js
@@ -0,0 +1,12 @@
+export const demoTodos = [
+  {
+    id: 1,
+    desc: 'Aprender React',
+    done: false,
+  },
+  {
+    id: 2,
+    desc: 'Aprender Mongo',
+    don: false,
+  },
+]
