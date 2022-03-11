f3a7f86844401502237f693dfff739ef163e1186
ElfGod <albertogx@icloud.com>
2022-03-11 15:28:43 -0500

    85: Pruebas restantes de mi Reducer

--- 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
+++ 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
@@ -23,4 +23,26 @@ describe('Pruebas en todoReducer', () => {
     expect(state.length).toBe(3)
     expect(state).toEqual([...demoTodos, newTodo])
   })
+  test('debe de borrar un TODO', () => {
+    const action = {
+      type: 'delete',
+      payload: 1,
+    }
+
+    const state = todoReducer(demoTodos, action)
+
+    expect(state.length).toBe(1)
+    expect(state).toEqual([demoTodos[1]])
+  })
+  test('debe de hacer el TOGGLE del TODO', () => {
+    const action = {
+      type: 'toggle',
+      payload: 1,
+    }
+
+    const state = todoReducer(demoTodos, action)
+    console.log(state)
+    expect(state[0].done).toBe(true)
+    expect(state[1]).toEqual(demoTodos[1])
+  })
 })
