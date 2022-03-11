753b421bac8ea26595629bfe070c3893df65adfc
ElfGod <albertogx@icloud.com>
2022-03-11 17:58:09 -0500

    87: Pruebas en el componente TodoListItem

---
+++ 05-hook-app/src/tests/components/08-useReducer/__snapshots__/TodoList.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TodoList from '../../../../components/08-useReducer/TodoList'
+import { demoTodos } from '../../../fixtures/demoTodos'
+
+describe('Pruebas en el <TodoList />', () => {
+  const handleDelete = jest.fn()
+  const handleToggle = jest.fn()
+
+  const wrapper = shallow(
+    <TodoList
+      todos={demoTodos}
+      handleDelete={handleDelete}
+      handleToggle={handleToggle}
+    />
+  )
+  test('debe de mostrarse correctamente', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+  test('debe de tener dos <TodoListItem />', () => {
+    console.log(wrapper.html())
+    expect(wrapper.find('TodoListItem').length).toBe(demoTodos.length)
+
+    console.log(wrapper.find('TodoListItem').at(0).props())
+    // console.log(wrapper.find('TodoListItem').at(0).prop('handleDelete'))
+    expect(wrapper.find('TodoListItem').at(0).prop('handleDelete')).toEqual(
+      expect.any(Function)
+    )
+  })
+})
