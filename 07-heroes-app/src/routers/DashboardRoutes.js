30138b94b124d0fa6610f0abf97a8391de7a3c69
ElfGod <albertogx@icloud.com>
2022-03-16 00:15:24 -0500

    99: Lista de Heroes

--- 07-heroes-app/src/routers/DashboardRoutes.js
+++ 07-heroes-app/src/routers/DashboardRoutes.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Navbar } from '../components/ui/NavBar'
 import { DcScreen } from '../components/dc/DcScreen'
-import { HeroScreen } from '../components/hero/HeroScreen'
+import { HeroScreen } from '../components/heroes/HeroScreen'
 import { MarvelScreen } from '../components/marvel/MarvelScreen'
 import { SearchScreen } from '../components/search/SearchScreen'
 
@@ -9,13 +9,15 @@ export const DashboardRoutes = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='marvel' element={<MarvelScreen />} />
-        <Route path='dc' element={<DcScreen />} />
-        <Route path='search' element={<SearchScreen />} />
-        <Route path='hero' element={<HeroScreen />} />
-        <Route path='/' element={<MarvelScreen />} />
-      </Routes>
+      <div className='container'>
+        <Routes>
+          <Route path='marvel' element={<MarvelScreen />} />
+          <Route path='dc' element={<DcScreen />} />
+          <Route path='search' element={<SearchScreen />} />
+          <Route path='hero' element={<HeroScreen />} />
+          <Route path='/' element={<MarvelScreen />} />
+        </Routes>
+      </div>
     </>
   )
 }
