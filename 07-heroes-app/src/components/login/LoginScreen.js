dd767a4720dc406bacdc5a7469144c7027aad0df
ElfGod <albertogx@icloud.com>
2022-03-15 22:02:51 -0500

    98: Navigate push / replace - useNavigate

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const LoginScreen = () => {
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true,
+    })
+  }
   return (
-    <div>
-      <h1>LoginScreen</h1>
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr />
+
+      <button className='btn btn-primary' onClick={handleLogin}>
+        Login
+      </button>
     </div>
   )
 }
--- 07-heroes-app/src/components/ui/NavBar.js
+++ 07-heroes-app/src/components/ui/NavBar.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
+  const navigate = useNavigate()
+
   const handleLogout = () => {
     // TODO: por hacer
     console.log('logout')
+    navigate('/login', {
+      replace: true,
+    })
   }
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
