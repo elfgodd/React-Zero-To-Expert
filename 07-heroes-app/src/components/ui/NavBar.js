a542b3820cce318eaab4faf178b518bdb8f49696
ElfGod <albertogx@icloud.com>
2022-03-15 20:42:51 -0500

    96: Colocar clase de la ruta activa

--- 07-heroes-app/src/components/ui/NavBar.js
+++ 07-heroes-app/src/components/ui/NavBar.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+  const handleLogout = () => {
+    // TODO: por hacer
+    console.log('logout')
+  }
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
       <Link className='navbar-brand' to='/'>
@@ -10,36 +14,26 @@ export const Navbar = () => {
 
       <div className='navbar-collapse'>
         <div className='navbar-nav'>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/marvel'
-          >
+          <NavLink className='nav-item nav-link' to='/marvel'>
             Marvel
           </NavLink>
 
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/dc'
-          >
+          <NavLink className='nav-item nav-link' to='/dc'>
             DC
           </NavLink>
         </div>
       </div>
 
-      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
+      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
+          <span className='nav-item nav-link text-info'>ElfGod</span>
+          <button
+            className='nav-item nav-link btn'
+            onClick={handleLogout}
             to='/login'
           >
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
