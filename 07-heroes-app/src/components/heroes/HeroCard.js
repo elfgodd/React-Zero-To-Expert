50ebe0e74835db269b1ac40c6746e4f9ff274f1e
ElfGod <albertogx@icloud.com>
2022-03-16 20:22:27 -0500

    100: Tarjetas con la información del Héroe

---
+++ 07-heroes-app/src/components/heroes/HeroCard.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom'
+
+export const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  const imagePath = `/assets/${id}.jpg`
+  return (
+    <div className='col'>
+      <div className='card'>
+        <div className='row no-gutters'>
+          <div className='col-4'>
+            <img src={imagePath} className='card-img-top' alt={superhero} />
+          </div>
+          <div className='col-8'>
+            <div className='card-body'>
+              <h5 className='card-title'>{superhero}</h5>
+              <p className='card-text'>{alter_ego}</p>
+              {alter_ego !== characters && (
+                <p className='text-muted'>{characters}</p>
+              )}
+              <Link to={`/hero/${id}`}>Mas...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- 07-heroes-app/src/components/heroes/HeroList.js
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -1,16 +1,15 @@
 import React from 'react'
+import { HeroCard } from './HeroCard'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher)
 
   return (
-    <>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.id}>{hero.superhero}</li>
-        ))}
-      </ul>
-    </>
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
   )
 }
