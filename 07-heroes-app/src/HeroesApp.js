1869bd69a29a5c0c5f3914d1b8cd366259cde68f
ElfGod <albertogx@icloud.com>
2022-03-18 15:53:04 -0500

    111: Login de un usuario

--- 07-heroes-app/src/HeroesApp.js
+++ 07-heroes-app/src/HeroesApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { AuthContext } from './auth/authContext'
 import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
@@ -9,6 +9,11 @@ const init = () => {
 
 export const HeroesApp = () => {
   const [user, dispatch] = useReducer(authReducer, {}, init)
+
+  useEffect(() => {
+    if (!user) return
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [user])
 
   return (
     <AuthContext.Provider value={{ user, dispatch }}>
--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../auth/authContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = () => {
   const navigate = useNavigate()
+  const { dispatch } = useContext(AuthContext)
 
   const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: { name: 'ElfGod' },
+    }
+    dispatch(action)
+
     navigate('/marvel', {
       replace: true,
     })
