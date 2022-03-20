5572e57e4dab91cdf6c7b7f91a6db985b2465e1d
ElfGod <albertogx@icloud.com>
2022-03-19 20:17:30 -0500

    114: Rutas públicas

--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -3,11 +3,19 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { LoginScreen } from '../components/login/LoginScreen'
 import { DashboardRoutes } from './DashboardRoutes'
 import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginScreen />} />
+        <Route
+          path='/login'
+          element={
+            <PublicRoute>
+              <LoginScreen />
+            </PublicRoute>
+          }
+        />
         <Route
           path='/*'
           element={
---
+++ 07-heroes-app/src/routers/PublicRoute.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { AuthContext } from '../auth/authContext'
+
+export const PublicRoute = ({ children }) => {
+  const { user } = useContext(AuthContext)
+
+  return user.logged ? <Navigate to='/marvel' /> : children
+}
