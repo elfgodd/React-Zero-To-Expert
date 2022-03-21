18eb56431a70bd0da1e012aa73eb447941262541
ElfGod <albertogx@icloud.com>
2022-03-20 21:41:29 -0500

    121: Probar rutas dentro del DashboardRoutes

---
+++ 07-heroes-app/src/test/components/search/SearchScreen.test.js
@@ -0,0 +1,15 @@
+import { mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
+import { SearchScreen } from '../../../components/search/SearchScreen'
+
+describe('Pruebas en <SearchScreen />', () => {
+  test('debe de mostrarse correctamente con valores por defecto', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search']}>
+        <SearchScreen />
+      </MemoryRouter>
+    )
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('.alert-info').text().trim()).toBe('Buscar un heroe')
+  })
+})
