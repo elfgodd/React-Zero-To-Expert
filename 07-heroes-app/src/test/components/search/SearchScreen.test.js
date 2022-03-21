01e066df6c392c35e31398c12d9a7238874d3a64
ElfGod <albertogx@icloud.com>
2022-03-20 21:52:10 -0500

    122: Pruebas con los queryParameters

--- 07-heroes-app/src/test/components/search/SearchScreen.test.js
+++ 07-heroes-app/src/test/components/search/SearchScreen.test.js
@@ -12,4 +12,23 @@ describe('Pruebas en <SearchScreen />', () => {
     expect(wrapper).toMatchSnapshot()
     expect(wrapper.find('.alert-info').text().trim()).toBe('Buscar un heroe')
   })
+  test('debe de mostrar a Batman y el input con el valor del queryString', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchScreen />
+      </MemoryRouter>
+    )
+    expect(wrapper.find('input').prop('value')).toBe('batman')
+    expect(wrapper).toMatchSnapshot()
+  })
+  test('debe de mostrar un error si no se encuentra el heroe', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman123']}>
+        <SearchScreen />
+      </MemoryRouter>
+    )
+    expect(wrapper.find('.alert-danger').text().trim()).toBe(
+      'No hay resultados: batman123'
+    )
+  })
 })
