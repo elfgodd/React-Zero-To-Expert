b814a0a8de2579b3ce1ed857ad845ea9bb770c7a
ElfGod <albertogx@icloud.com>
2022-03-20 22:13:45 -0500

    123: Mock de funciones y requireActual

--- 07-heroes-app/src/test/components/search/SearchScreen.test.js
+++ 07-heroes-app/src/test/components/search/SearchScreen.test.js
@@ -2,6 +2,13 @@ import { mount } from 'enzyme'
 import { MemoryRouter } from 'react-router-dom'
 import { SearchScreen } from '../../../components/search/SearchScreen'
 
+const mockNavigate = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}))
+
 describe('Pruebas en <SearchScreen />', () => {
   test('debe de mostrarse correctamente con valores por defecto', () => {
     const wrapper = mount(
@@ -30,5 +37,23 @@ describe('Pruebas en <SearchScreen />', () => {
     expect(wrapper.find('.alert-danger').text().trim()).toBe(
       'No hay resultados: batman123'
     )
+  })
+  test('debe de llamar el navigate a la nueva pantalla', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search']}>
+        <SearchScreen />
+      </MemoryRouter>
+    )
+    wrapper.find('input').simulate('change', {
+      target: {
+        name: 'searchText',
+        value: 'batman',
+      },
+    })
+    wrapper.find('form').prop('onSubmit')({
+      preventDefault() {},
+    })
+    // expect(mockNavigate).toHaveBeenCalledWith('Hola mundo') // Error
+    expect(mockNavigate).toHaveBeenCalledWith('?q=batman')
   })
 })
