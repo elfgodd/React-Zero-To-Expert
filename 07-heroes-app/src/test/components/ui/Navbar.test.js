9711cec7c7d5db52f7a1df37ea2bcf2c6ac2a5b5
ElfGod <albertogx@icloud.com>
2022-03-20 23:42:02 -0500

    125: Solución - Pruebas en el Navbar

--- 07-heroes-app/src/test/components/ui/Navbar.test.js
+++ 07-heroes-app/src/test/components/ui/Navbar.test.js
@@ -1,4 +1,40 @@
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
+import { mount } from 'enzyme'
+import { Navbar } from '../../../components/ui/NavBar'
+import { AuthContext } from '../../../auth/authContext'
+import { types } from '../../../types/types'
+
+const mockNavigate = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}))
+
 describe('Pruebas en <Navbar />', () => {
-  test('debe de mostrar correctamente', () => {})
-  test('debe de llamar el logout, llamar el navigate y el dispatch con los argumentos', () => {})
+  const contextValue = {
+    dispatch: jest.fn(),
+    user: { name: 'ElfGod', logged: true },
+  }
+
+  const wrapper = mount(
+    <AuthContext.Provider value={contextValue}>
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path='/' element={<Navbar />} />
+        </Routes>
+      </MemoryRouter>
+    </AuthContext.Provider>
+  )
+  test('debe de mostrar correctamente', () => {
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find('.text-info').text().trim()).toBe('ElfGod')
+  })
+  test('debe de llamar el logout, llamar el navigate y el dispatch con los argumentos', () => {
+    wrapper.find('button').prop('onClick')()
+    expect(contextValue.dispatch).toHaveBeenCalledWith({
+      type: types.logout,
+    })
+    expect(mockNavigate).toHaveBeenCalledWith('/login', { replace: true })
+  })
 })
