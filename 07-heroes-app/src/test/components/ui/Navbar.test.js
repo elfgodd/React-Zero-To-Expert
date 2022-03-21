23af789348d20593dde41abcaa40c37398ba7cc8
ElfGod <albertogx@icloud.com>
2022-03-20 22:28:51 -0500

    124: Tarea - Pruebas en el Navbar

---
+++ 07-heroes-app/src/test/components/ui/Navbar.test.js
@@ -0,0 +1,4 @@
+describe('Pruebas en <Navbar />', () => {
+  test('debe de mostrar correctamente', () => {})
+  test('debe de llamar el logout, llamar el navigate y el dispatch con los argumentos', () => {})
+})
