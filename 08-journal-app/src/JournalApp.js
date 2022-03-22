3c912f99842a400927928c798f463e953ba87158
ElfGod <albertogx@icloud.com>
2022-03-21 22:25:20 -0500

    133: Implementando rutas principales y rutas hijas

--- 08-journal-app/src/JournalApp.js
+++ 08-journal-app/src/JournalApp.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './routers/AppRouter'
 
 export const JournalApp = () => {
   return (
     <>
-      <h1>JournalApp</h1>
+      <AppRouter />
     </>
   )
 }
---
+++ 08-journal-app/src/routers/AuthRouter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { LoginScreen } from '../components/auth/LoginScreen'
+import { RegisterScreen } from '../components/auth/RegisterScreen'
+export const AuthRouter = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/auth/login' component={LoginScreen} />
+        <Route exact path='/auth/register' component={RegisterScreen} />
+        <Redirect to='/auth/login' />
+      </Switch>
+    </div>
+  )
+}
