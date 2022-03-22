3d35e362e64d8b8c7086055706f249cac3f02dd1
ElfGod <albertogx@icloud.com>
2022-03-22 17:37:56 -0500

    Estilos del AuthRouter y LoginScreen

--- 08-journal-app/src/routers/AuthRouter.js
+++ 08-journal-app/src/routers/AuthRouter.js
@@ -4,12 +4,14 @@ import { LoginScreen } from '../components/auth/LoginScreen'
 import { RegisterScreen } from '../components/auth/RegisterScreen'
 export const AuthRouter = () => {
   return (
-    <div>
-      <Switch>
-        <Route exact path='/auth/login' component={LoginScreen} />
-        <Route exact path='/auth/register' component={RegisterScreen} />
-        <Redirect to='/auth/login' />
-      </Switch>
+    <div className='auth__main'>
+      <div className='auth__box-container'>
+        <Switch>
+          <Route exact path='/auth/login' component={LoginScreen} />
+          <Route exact path='/auth/register' component={RegisterScreen} />
+          <Redirect to='/auth/login' />
+        </Switch>
+      </div>
     </div>
   )
 }
