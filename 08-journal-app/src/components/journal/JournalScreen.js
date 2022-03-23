83ed86cb0f23486b6505ca353b8e15cf8434640e
ElfGod <albertogx@icloud.com>
2022-03-22 22:04:15 -0500

    137: Componente cuando no hay nada seleccionado

--- 08-journal-app/src/components/journal/JournalScreen.js
+++ 08-journal-app/src/components/journal/JournalScreen.js
@@ -1,13 +1,12 @@
 import React from 'react'
+// import { NothingSelected } from './NothingSelected'
 import { Sidebar } from './Sidebar'
 
 export const JournalScreen = () => {
   return (
     <div className='journal__main-content'>
       <Sidebar />
-      <main>
-        <h1>Main content</h1>
-      </main>
+      <main>{/* <NothingSelected /> */}</main>
     </div>
   )
 }
