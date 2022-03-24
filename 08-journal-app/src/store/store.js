883306291a36f99fb0ee85cb697663b0e70ccbb8
ElfGod <albertogx@icloud.com>
2022-03-23 23:54:01 -0500

    138: Redux DevTools

--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -5,4 +5,9 @@ const reducers = combineReducers({
   auth: authReducer,
 })
 
-export const store = createStore(reducers)
+/* eslint-disable no-underscore-dangle */
+export const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+/* eslint-enable */
