98274d5b6b04555004edfb78282b8f35690f2c4b
ElfGod <albertogx@icloud.com>
2022-03-24 00:13:53 -0500

    139: Primer dispatch de una acción a nuestro Store

---
+++ 08-journal-app/src/actions/auth.js
@@ -0,0 +1,6 @@
+import { types } from '../types/types'
+
+export const login = (uid, displayName) => ({
+  type: types.login,
+  payload: { uid, displayName },
+})
--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -1,23 +1,44 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { login } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+  const dispatch = useDispatch()
+
+  const [formValues, handleInputChange, reset] = useForm({
+    email: 'elfgod@gmail.com',
+    password: '123456',
+  })
+
+  const { email, password } = formValues
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+    dispatch(login(12345, 'Josue'))
+  }
   return (
     <>
       <h3 className='auth__title'>Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type='text'
           placeholder='Email'
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type='password'
           placeholder='Password'
           name='password'
           className='auth__input'
+          value={password}
+          onChange={handleInputChange}
         />
         <button type='submit' className='btn btn-primary btn-block'>
           Login
