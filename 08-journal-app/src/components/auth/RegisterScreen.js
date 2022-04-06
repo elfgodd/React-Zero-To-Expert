3394b2e6594eddc535d65dbb3b41d2981f0fdae6
ElfGod <albertogx@icloud.com>
2022-04-06 00:30:58 -0500

    143: Formulario de registro de usuarios

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -1,17 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
 
 export const RegisterScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    name: 'ElfGod',
+    email: 'elfgod@gmail.com',
+    password: '123456',
+    password2: '123456',
+  })
+  const { name, email, password, password2 } = formValues
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    console.log(formValues)
+    // console.log(name, email, password, password2)
+  }
+
   return (
     <>
       <h3 className='auth__title'>Register</h3>
-      <form>
+      <form onSubmit={handleRegister}>
         <input
           type='text'
           placeholder='Name'
           name='name'
           className='auth__input'
           autoComplete='off'
+          value={name}
+          onChange={handleInputChange}
         />
         <input
           type='text'
@@ -19,18 +36,24 @@ export const RegisterScreen = () => {
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type='password'
           placeholder='Password'
           name='password'
           className='auth__input'
+          value={password}
+          onChange={handleInputChange}
         />
         <input
           type='password'
           placeholder='Confirm password'
           name='password2'
           className='auth__input'
+          value={password2}
+          onChange={handleInputChange}
         />
         <button type='submit' className='btn btn-primary btn-block mb-5'>
           Register
