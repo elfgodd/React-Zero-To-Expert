2ceeda0e2365fcdaeda6253cf1ee388797bf9608
ElfGod <albertogx@icloud.com>
2022-04-08 00:12:50 -0500

    NotesReducer

---
+++ 08-journal-app/src/reducers/notesReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  notes: [],
+  active: null,
+}
+
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import { authReducer } from '../reducers/authReducer'
+import { notesReducer } from '../reducers/notesReducer'
 import { uiReducer } from '../reducers/uiReducer'
 
 const composeEnhancers =
@@ -12,6 +13,7 @@ const composeEnhancers =
 const reducers = combineReducers({
   auth: authReducer,
   ui: uiReducer,
+  notes: notesReducer,
 })
 
 export const store = createStore(
