8b1a5fe648ac359e11ac9cf93d1d1914b93a4d98
ElfGod <albertogx@icloud.com>
2022-04-07 23:56:27 -0500

    Continuación del proyecto - JournalApp

--- 08-journal-app/src/components/journal/Sidebar.js
+++ 08-journal-app/src/components/journal/Sidebar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { JournalEntries } from './JournalEntries'
 import { startLogout } from '../../actions/auth'
 
 export const Sidebar = () => {
   const dispatch = useDispatch()
-
+  const { name } = useSelector((state) => state.auth)
   const handleLogout = () => {
     console.log('click')
     dispatch(startLogout())
@@ -16,7 +16,7 @@ export const Sidebar = () => {
       <div className='journal__sidebar-navbar'>
         <h3 className='mt-5'>
           <i className='far fa-moon'></i>
-          <span> ElfGod</span>
+          <span>{name}</span>
         </h3>
         <button className='btn' onClick={handleLogout}>
           Logout
