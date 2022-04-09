35b1f6c3559f0fff7f9991390ff9fc81b940dadd
ElfGod <albertogx@icloud.com>
2022-04-09 16:40:50 -0500

    Purgar las notas en un logout

--- 08-journal-app/src/actions/notes.js
+++ 08-journal-app/src/actions/notes.js
@@ -21,11 +21,17 @@ export const startNewNote = () => {
     const doc = await db.collection(`${uid}/journal/notes`).add(newNote)
     // console.log(doc)
     dispatch(activeNote(doc.id, newNote))
+    dispatch(addNewNote(doc.id, newNote))
   }
 }
 
 export const activeNote = (id, note) => ({
   type: types.notesActive,
+  payload: { id, ...note },
+})
+
+export const addNewNote = (id, note) => ({
+  type: types.notesAddNew,
   payload: { id, ...note },
 })
 
@@ -104,4 +110,8 @@ export const startDeleting = (id) => {
 export const deleteNote = (id) => ({
   type: types.notesDelete,
   payload: id,
+})
+
+export const noteLogout = () => ({
+  type: types.notesLogoutCleaning,
 })
--- 08-journal-app/src/reducers/notesReducer.js
+++ 08-journal-app/src/reducers/notesReducer.js
@@ -12,6 +12,11 @@ export const notesReducer = (state = initialState, action) => {
         ...state,
         active: { ...action.payload },
       }
+    case types.notesAddNew:
+      return {
+        ...state,
+        notes: [action.payload, ...state.notes],
+      }
     case types.notesLoad:
       return { ...state, notes: [...action.payload] }
     case types.notesUpdated:
@@ -27,6 +32,8 @@ export const notesReducer = (state = initialState, action) => {
         active: null,
         notes: state.notes.filter((note) => note.id !== action.payload),
       }
+    case types.notesLogoutCleaning:
+      return { ...state, active: null, notes: [] }
     default:
       return state
   }
