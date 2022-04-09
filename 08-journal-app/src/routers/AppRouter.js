4c06da1cff5d016fbd28a707083fbf23a2def1ba
ElfGod <albertogx@icloud.com>
2022-04-08 21:06:31 -0500

    Optimización de la petición de carga de notas

--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -8,8 +8,7 @@ import { login } from '../actions/auth'
 import { useDispatch } from 'react-redux'
 import { PrivateRoute } from './PrivateRoute'
 import { PublicRoute } from './PublicRoute'
-import { loadNotes } from '../helpers/loadNotes'
-import { setNotes } from '../actions/notes'
+import { startLoadingNotes } from '../actions/notes'
 
 export const AppRouter = () => {
   const dispatch = useDispatch()
@@ -23,8 +22,7 @@ export const AppRouter = () => {
         dispatch(login(user.uid, user.displayName))
         setIsLoggedIn(true)
 
-        const notes = await loadNotes(user.uid)
-        dispatch(setNotes(notes))
+        dispatch(startLoadingNotes(user.uid))
       } else {
         setIsLoggedIn(false)
       }
