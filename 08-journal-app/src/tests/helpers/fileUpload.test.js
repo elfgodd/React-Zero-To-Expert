1e359f5244c12cbb6df14ba4c9a7cef516dde166
ElfGod <albertogx@icloud.com>
2022-04-10 23:35:35 -0500

    Cloudinary SDK - Delete image

--- 08-journal-app/src/tests/helpers/fileUpload.test.js
+++ 08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -6,10 +6,11 @@ cloudinary.config({
   cloud_name: 'alienroom',
   api_key: '657426181456785',
   api_secret: 'Nr2jtLglUazIZad0CLLJQdD4OWQ',
+  secure: true,
 })
 
 describe('Pruebas en fileUpload', () => {
-  test('debe de cargar un archivo y retornar el URL', async (done) => {
+  test('debe de cargar un archivo y retornar el URL', async () => {
     const resp = await fetch(
       'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'
     )
@@ -22,10 +23,11 @@ describe('Pruebas en fileUpload', () => {
 
     // Borrar imagen por ID
     const segments = url.split('/')
+    console.log(segments)
     const imageId = segments[segments.length - 1].replace('.png', '')
 
-    cloudinary.v2.api.delete_resources(imageId, {}, () => {
-      done()
+    await cloudinary.v2.uploader.destroy(imageId, {}, () => {
+      console.log('Imagen borrada')
     })
   })
 
