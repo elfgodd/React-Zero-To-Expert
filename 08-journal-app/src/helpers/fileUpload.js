37298aa80f764f58975222341b3c35920e5ece44
ElfGod <albertogx@icloud.com>
2022-04-10 23:03:41 -0500

    Pruebas de carga de archivos

--- 08-journal-app/src/helpers/fileUpload.js
+++ 08-journal-app/src/helpers/fileUpload.js
@@ -15,7 +15,8 @@ export const fileUpload = async (file) => {
       const cloudResp = await resp.json()
       return cloudResp.secure_url
     } else {
-      throw await resp.json()
+      // throw await resp.json()
+      return null
     }
   } catch (err) {
     throw err
