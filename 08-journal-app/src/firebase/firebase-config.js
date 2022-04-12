f423eb63de3d7a365b0cfeb1eeabed2a373f3a60
ElfGod <albertogx@icloud.com>
2022-04-11 23:32:33 -0500

    Variables de entorno

--- 08-journal-app/src/firebase/firebase-config.js
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -2,31 +2,23 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
+// console.log(process.env)
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDPWqH6P_oOvDu73X-u-fd1njumvxyFKRM',
-
-  authDomain: 'react-app-courses-1cfb4.firebaseapp.com',
-
-  projectId: 'react-app-courses-1cfb4',
-
-  storageBucket: 'react-app-courses-1cfb4.appspot.com',
-
-  messagingSenderId: '96326384956',
-
-  appId: '1:96326384956:web:2bd4a20d6c4fdfa5e53239',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 }
 
 const firebaseConfigTesting = {
   apiKey: 'AIzaSyDceIDIAd0oJbsXnThtZwN6ricgNUH25wc',
-
   authDomain: 'redux-test-6cee8.firebaseapp.com',
-
   projectId: 'redux-test-6cee8',
-
   storageBucket: 'redux-test-6cee8.appspot.com',
-
   messagingSenderId: '851856973528',
-
   appId: '1:851856973528:web:ff81568e3e370b7434c1ad',
 }
 
