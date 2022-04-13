bd41eace2ffdca47079417d8603e6dbc0bc12365
ElfGod <albertogx@icloud.com>
2022-04-12 20:33:45 -0500

    React Big Calendar

--- 09-calendar-app/src/index.js
+++ 09-calendar-app/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import CalendarApp from './CalendarApp'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <BrowserRouter>
-    <CalendarApp />
-  </BrowserRouter>
-)
+import './styles.css'
+
+ReactDOM.render(<CalendarApp />, document.getElementById('root'))
