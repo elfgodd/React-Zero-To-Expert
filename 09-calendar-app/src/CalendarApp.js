c6e9984db2fb86fb173d1784163ef7743495ff11
ElfGod <albertogx@icloud.com>
2022-04-12 19:07:12 -0500

    Inicio de proyecto - MERN-Calendar

---
+++ 09-calendar-app/src/CalendarApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppRouter from './router/AppRouter'
+
+const CalendarApp = () => {
+  return (
+    <>
+      <AppRouter>CalendarApp</AppRouter>
+    </>
+  )
+}
+
+export default CalendarApp
---
+++ 09-calendar-app/src/router/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import LoginScreen from '../components/auth/LoginScreen'
+import CalendarScreen from '../components/calendar/CalendarScreen'
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={CalendarScreen} />
+          <Route exact path='/login' component={LoginScreen} />
+
+          <Redirect to='/' />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default AppRouter
