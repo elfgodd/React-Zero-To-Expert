d2884ec1fc07f6abc572306bf917e2b9fb8300af
ElfGod <albertogx@icloud.com>
2022-04-12 19:36:39 -0500

    LoginScreen y Navbar

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../ui/Navbar'
 
 const CalendarScreen = () => {
   return (
     <div>
-      <h1>CalendarScreen</h1>
+      <Navbar />
     </div>
   )
 }
