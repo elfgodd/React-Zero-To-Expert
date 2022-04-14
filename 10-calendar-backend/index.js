097c0725ae97a858aa9f1840b01d1d0f5ed47a55
ElfGod <albertogx@icloud.com>
2022-04-14 11:33:05 -0500

    Configurando Express

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,17 @@
-console.log('hola mundo!!')
+const express = require('express')
+
+// Crear el servidor de express
+const app = express()
+
+// Rutas
+app.get('/', (req, res) => {
+  console.log('se quiere /')
+  res.json({
+    ok: true,
+  })
+})
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en el puerto ${4000}`)
+})
